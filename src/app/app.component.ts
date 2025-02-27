d73a5a2e261d8579db35acd6a763457e3059d088
Fuzzy39 <mshill126@gmail.com>
2025-02-27 10:11:36 -0500

    added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ResourceComponent } from './resource/resource.component';
+import { ResourceContainerComponent } from './resource-container/resource-container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ResourceComponent, ResourceContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
