f510edcac833c73f8bee6b951ba3c22b626a8061
Fuzzy39 <mshill126@gmail.com>
2025-02-27 10:12:37 -0500

    okay third try

---
+++ src/app/resource-container/resource-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ResourceComponent } from '../resource/resource.component';
+
+@Component({
+  selector: 'app-resource-container',
+  imports: [ResourceComponent],
+  templateUrl: './resource-container.component.html',
+  styleUrl: './resource-container.component.css'
+})
+export class ResourceContainerComponent {
+
+}
